3f75b20178c71f9e8a8b56f0f15a42d08741ffba
robertburrowes <r.burrowes@auckland.ac.nz>
2020-10-25 21:13:26 +1300

    mv dev scripts to sbin

--- genpkg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#Local checking. Creates pkg/
-./gendoc.sh
-/usr/local/bin/rake --trace gem
-
--- local_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#Need to run from command line.
-sudo /usr/local/bin/gem install pkg/wikk_sql-0.1.0.gem
-
