4cd99e2968ad8baf708b08b05119fd924349005e
robertburrowes <r.burrowes@auckland.ac.nz>
2022-05-26 16:55:35 +1200

    Working towards having the gem work with either ruby-mysql or mysql2

--- lib/wikk_sql.rb
+++ lib/wikk_sql.rb
@@ -1,276 +1,44 @@
+# Provides common front end, even if we change the connector library
+# Choice of ruby-mysql or mysql2 connectors
 module WIKK
-  require 'mysql'
   require 'pp'
   require 'json'
 
-  # WIKK_SQL wrapper for ruby mysql gem.
-  # @attr_reader [Numeric] affected_rows the number of rows changed, deleted, or added.
-  # @attr_reader [Mysql::Result] result the last query's result
-  # @attr_reader [Mysql] my the DB connection descriptor
+  # From mysql.h, but mostly blank for both our connectors
+  MySQL_FIELD = Struct.new(
+    :name,        # Field name. Could be an 'as' alias
+    :org_name,    # Field name, not aliased (for introspection)
+    :table,       # Table name. Could be an 'as' alias
+    :org_table,   # Table name, not aliased (for introspection)
+    :db,          # database for table (useful for multitable queries)
+    :catalog,     # catalog for table
+    :def,         # Default value (if code wants to revert to the default)
+    :col_width,   # Width of column (length in mysql.h, but length is taken in Ruby)
+    :max_length,  # Max width for selected set
+    :flags,       # Div flags
+    :decimals,    # Number of decimals in field
+    :charsetnr,   # Character set
+    :type,        # Field type
+    :extension
+  )
+
   class SQL
-    VERSION = '0.1.4'
+    VERSION = '0.2.0'
+  end
 
-    attr_reader :affected_rows, :result, :my
-
-    # Create WIKK::SQL instance and set up the mySQL connection.
-    # @param db_config [Configuration]  Configuration class, Hash, or any class with appropriate attr_readers.
-    # @yieldparam sql [WIKK_SQL] if a block is given.
-    # @return [NilClass] if block is given, and closes the mySQL connection.
-    # @return [WIKK_SQL] if no block is given, and caller must call sql.close
-    def self.connect(db_config)
-      sql = self.new
-      sql.connect(db_config)
-      if block_given?
-        yield sql
-        return sql.close
-      else
-        return sql
-      end
+  # Prefer mysql2 over ruby-mysql, as it is faster
+  # Downside is, mysql2 doesn't expose field table names.
+  begin
+    Gem::Specification.find_by_name('mysql2')
+  rescue Gem::LoadError
+    begin
+      Gem::Specification.find_by_name('ruby-sql')
+    rescue Gem::LoadError
+      raise Gem::LoadError 'Need either mysql2 or ruby-mysql gems'
+    else
+      require_relative 'wikk_ruby_mysql.rb'
     end
-
-    # Set up the mySQL connection.
-    #
-    # @param db_config [Configuration]  Configuration class, Hash, or any class with appropriate attr_readers.
-    # @yieldparam [] if a block is given.
-    # @return [NilClass] if block is given, and closes the mySQL connection.
-    # @return [WIKK_SQL] if no block is given, and caller must call sql.close
-    def connect(db_config)
-      if db_config.instance_of?(Hash)
-        sym = db_config.each_with_object({}) { |(k, v), h| h[k.to_sym] = v }
-        db_config = Struct.new(*(k = sym.keys)).new(*sym.values_at(*k))
-      end
-
-      begin
-        @my = Mysql.new(db_config.host, db_config.dbuser, db_config.key, db_config.db )
-      rescue Exception => e
-        @my = nil
-        raise e
-      end
-      raise Mysql::Error, 'Not Connected' if @my.nil?
-
-      # @@my.reconnect = true
-      if block_given?
-        yield
-        return close
-      end
-      return @my
-    end
-
-    alias open connect
-
-    # close the mySQL connection. Call only if connect was not given a block.
-    #
-    # @return [NilClass]
-    def close
-      @my.close if @my != nil
-      return (@my = nil)
-    end
-
-    # Run a query on the DB server.
-    #
-    # @param the_query [String]  Sql query to send to DB server.
-    # @raise [Mysql] passes on Mysql errors, freeing the result.
-    # @yieldparam [Mysql::Result] @result and @affected_rows are also set.
-    # @return [Mysql::Result] @result and @affected_rows are also set.
-    def query(the_query)
-      raise Mysql::Error, 'Not Connected' if @my.nil?
-
-      begin
-        if @result != nil
-          @result.free # Free any result we had left over from previous use.
-          @result = nil
-        end
-        @affected_rows = 0 # incase this query crashes and burns, this will have a value.
-        @result = @my.query(the_query)
-        @affected_rows = @my.affected_rows # This is non-zero for insert/delete/update of rows
-        if block_given?
-          yield @result
-        else
-          return @result
-        end
-      rescue Mysql::Error => e
-        if @result != nil
-          @result.free # Free any result we had left over from previous use.
-          @result = nil
-        end
-        raise e
-      end
-    end
-
-    # Perform a transaction in the passed block.
-    # RollBACK on error, otherwise COMMIT
-    #
-    # @yieldparam [] yields to block, where the queries are performed.
-    # @raise [Mysql] passes on Mysql errors, freeing the result.
-    def transaction
-      raise Mysql::Error, 'Not Connected' if @my.nil?
-
-      if block_given?
-        begin
-          @my.query('START TRANSACTION WITH CONSISTENT SNAPSHOT')
-          yield # Start executing the query black.
-          @my.query('COMMIT')
-        rescue Mysql::Error => e
-          @my.query('ROLLBACK')
-          raise e
-        end
-      end
-    end
-
-    # Yields query query results row by row, as Array
-    #
-    # @param the_query [String]  Sql query to send to DB server.
-    # @raise [Mysql] passes on Mysql errors, freeing the result.
-    # @yieldparam [Array] each result row
-    # @note @result and @affected_rows are also set via call to query().
-    def each_row(the_query, &block)
-      begin
-        query(the_query)
-        if @result != nil && block_given?
-          @affected_rows = @result.num_rows # This is non-zero is we do a select, and get results.
-          @result.each(&block)
-        end
-      rescue Mysql::Error => e
-        # puts "#{e.errno}: #{e.error}"
-        raise e
-      ensure
-        if block_given? && @result != nil
-          @result.free
-        end
-      end
-    end
-
-    # Yields query result row by row, as Hash, using String keys
-    #
-    # @param the_query [String]  Sql query to send to DB server.
-    # @param with_table_names [Boolean] if TrueClass, then table names are included in the hash keys.
-    # @raise [Mysql] passes on Mysql errors, freeing the result.
-    # @yieldparam [Hash] each result row
-    # @note @result and @affected_rows are also set via call to query().
-    def each_hash(the_query, with_table_names = false, &block)
-      begin
-        query(the_query)
-        if @result != nil && block_given?
-          @affected_rows = @result.num_rows # This is non-zero is we do a select, and get results.
-          @result.each_hash(with_table_names, &block)
-        end
-      rescue Mysql::Error => e
-        # puts "#{e.errno}: #{e.error}"
-        raise e
-      ensure
-        if block_given? && @result != nil
-          @result.free
-        end
-      end
-    end
-
-    # Yields query result row by row, as Hash using Symbol keys, so can't have table names included.
-    # This can be used with keyword arguments. eg. each_sym { |key1:, key2:, ..., **rest_of_args| do something }
-    #
-    # @param the_query [String]  Sql query to send to DB server.
-    # @raise [Mysql] passes on Mysql errors, freeing the result.
-    # @yieldparam [Hash] each result row
-    # @note @result and @affected_rows are also set via call to query().
-    def each_sym(the_query)
-      each_hash(the_query) do |row_hash|
-        yield row_hash.each_with_object({}) { |(k, v), h| h[k.to_sym] = v }
-      end
-    end
-
-    # Get the database field attributes from a query result.
-    #
-    # @yieldparam [Array][Mysql::Field] Array of field records
-    # @note fields are name (of field), table (name), def, type, length, max_length, flags,decimals
-    def fetch_fields
-      @result.fetch_fields
-    end
-
-    # Create WIKK::SQL instance and set up the mySQL connection, and Run a query on the DB server.
-    #
-    # @param db_config [Configuration]  Configuration class, Hash, or any class with appropriate attr_readers.
-    # @param the_query [String]  Sql query to send to DB server.
-    # @raise [Mysql] passes on Mysql errors, freeing the result.
-    # @yieldparam [Mysql::Result] @result and @affected_rows are also set.
-    # @return [Mysql::Result] @result and @affected_rows are also set.
-    def self.query(db_config, the_query)
-      sql = self.new
-      sql.open db_config
-      begin
-        result = sql.query(the_query)
-        if block_given?
-          yield result
-        end
-      ensure
-        sql.close
-      end
-      return result
-    end
-
-    # Create WIKK::SQL instance and set up the mySQL connection, and Run a query on the DB server.
-    # Yields query query results row by row, as Array
-    #
-    # @param db_config [Configuration]  Configuration class, Hash, or any class with appropriate attr_readers.
-    # @param the_query [String]  Sql query to send to DB server.
-    # @raise [Mysql] passes on Mysql errors, freeing the result.
-    # @yieldparam [Array] each result row
-    # @note @result and @affected_rows are also set via call to query().
-    def self.each_row(db_config, query, &block)
-      sql = self.new
-      sql.open db_config
-      begin
-        if block_given?
-          sql.each_row(query, &block)
-        end
-      ensure
-        sql.close
-      end
-      return sql
-    end
-
-    # Create WIKK::SQL instance and set up the mySQL connection, and Run a query on the DB server.
-    # Yields query result row by row, as Hash, using String keys
-    #
-    # @param db_config [Configuration]  Configuration class, Hash, or any class with appropriate attr_readers.
-    # @param the_query [String]  Sql query to send to DB server.
-    # @param with_table_names [Boolean] if TrueClass, then table names are included in the hash keys.
-    # @raise [Mysql] passes on Mysql errors, freeing the result.
-    # @yieldparam [Hash] each result row
-    # @note @result and @affected_rows are also set via call to query().
-    def self.each_hash(db_config, query, with_table_names = false, &block)
-      sql = self.new
-      sql.open db_config
-      begin
-        if block_given?
-          sql.each_hash(query, with_table_names, &block)
-        end
-      ensure
-        sql.close
-      end
-      return sql
-    end
-
-    # Create WIKK::SQL instance and set up the mySQL connection, and Run a query on the DB server.
-    # Yields query result row by row, as Hash using Symbol keys, so can't have table names included.
-    #
-    # @param db_config [Configuration]  Configuration class, Hash, or any class with appropriate attr_readers.
-    # This can be used with keyword arguments. eg. each_sym { |key1:, key2:, ..., **rest_of_args| do something }
-    # @param the_query [String]  Sql query to send to DB server.
-    # @raise [Mysql] passes on Mysql errors, freeing the result.
-    # @yieldparam [Hash] each result row
-    # @note @result and @affected_rows are also set via call to query().
-    def self.each_sym(db_config, query)
-      sql = self.new
-      sql.open db_config
-      begin
-        if block_given?
-          sql.each_sym(query) do |**res|
-            yield(**res)
-          end
-        end
-      ensure
-        sql.close
-      end
-      return sql
-    end
+  else
+    require_relative 'wikk_mysql2.rb'
   end
 end
--- test/test_wikk_sql.rb
+++ test/test_wikk_sql.rb
@@ -1,70 +1,68 @@
 #!/usr/local/bin/ruby
-#Warning Site specific testing going on here!!!!!!!!!!
+# Warning Site specific testing going on here!!!!!!!!!!
 
-require_relative '../lib/wikk_sql.rb' #Local test
-#require "wikk_sql" #test installed gem
+require_relative '../lib/wikk_sql.rb' # Local test
+# require "wikk_sql" #test installed gem
 require 'wikk_configuration'
 
-@config = WIKK::Configuration.new('../../../etc/ntm_conf.json')
+@config = WIKK::Configuration.new(ARGV[0])
 
 def test_class_lvl_each_row
-  puts "test_class_lvl_each_row: select name, site_name from customer limit 2"
-  WIKK::SQL::each_row(@config, "select name, site_name  from customer limit 2") do |row|
+  puts 'test_class_lvl_each_row: select name, site_name from customer limit 2'
+  WIKK::SQL.each_row(@config, 'select name, site_name  from customer limit 2') do |row|
     printf "%s, %s\n", row[0], row[1]
   end
 end
 
 def test_class_lvl_each_hash
-  puts "test_class_lvl_each_hash: select * from customer limit 2"
-  WIKK::SQL::each_hash(@config, "select * from customer limit 2") do |row|
-    row.each do |k,v|
+  puts 'test_class_lvl_each_hash: select * from customer limit 2'
+  WIKK::SQL.each_hash(@config, 'select * from customer limit 2') do |row|
+    row.each do |k, v|
       printf "  %s => %s\n", k, v
     end
   end
 end
 
 def test_class_lvl_each_sym
-  puts "test_class_lvl_each_sym select * from customer limit 2"
-  WIKK::SQL::each_sym(@config, "select * from customer limit 2") do |customer_id:, name:, site_name:, **row|
+  puts 'test_class_lvl_each_sym select * from customer limit 2'
+  WIKK::SQL.each_sym(@config, 'select * from customer limit 2') do |customer_id:, name:, site_name:, **_row|
     printf "customer_id %s  site_name %s name %s\n", customer_id, site_name, name
   end
 end
 
 def test_class_lvl_each_hash_with_table_names
-  puts "test_class_lvl_each_hash_with_table_names: select * from customer limit 2"
-  WIKK::SQL::each_hash(@config, "select * from customer limit 2", with_table_names = true) do |row|
-    puts "Row"
-    row.each do |k,v|
+  puts 'test_class_lvl_each_hash_with_table_names: select * from customer limit 2'
+  WIKK::SQL.each_hash(@config, 'select * from customer limit 2', with_table_names = true) do |row|
+    puts 'Row'
+    row.each do |k, v|
       printf "  %s => %s\n", k, v
     end
   end
 end
 
-
-  
 def test_instance_lvl_get_fields
-  puts "test_instance_lvl_get_fields: select * from customer limit 1"
-  WIKK::SQL::connect(@config) do |sql|
-    sql.query("select * from customer limit 1") do |result|
-     # puts result.fetch_fields[0].class
+  puts 'test_instance_lvl_get_fields: select * from customer limit 1'
+  WIKK::SQL.connect(@config) do |sql|
+    sql.query('select * from customer limit 1') do |result|
+      # puts result.fetch_fields[0].class
       result.fetch_fields.each_with_index do |info, i|
-             printf "--- Column %d (%s) ---\n", i, info.name
-             printf "table:            %s\n", info.table
-             printf "def:              %s\n", info.def
-             printf "type:             %s\n", info.type
-             printf "length:           %s\n", info.length
-             printf "max_length:       %s\n", info.max_length
-             printf "flags:            %s\n", info.flags
-             printf "decimals:         %s\n", info.decimals
+        printf "--- Column %d (%s) ---\n", i, info.name
+        printf "table:            %s\n", info.table
+        printf "def:              %s\n", info.def
+        printf "type:             %s\n", info.type
+        printf "length:           %s\n", info.length
+        printf "max_length:       %s\n", info.max_length
+        printf "flags:            %s\n", info.flags
+        printf "decimals:         %s\n", info.decimals
       end
     end
   end
 end
 
 def test_instance_lvl_each_row
-  puts "test_instance_lvl_each_row: select name, site_name from customer limit 2"
-  WIKK::SQL::connect(@config) do |sql|
-    sql.each_row("select name, site_name from customer limit 2") do |row|
+  puts 'test_instance_lvl_each_row: select name, site_name from customer limit 2'
+  WIKK::SQL.connect(@config) do |sql|
+    sql.each_row('select name, site_name from customer limit 2') do |row|
       printf "%s, %s\n", row[0], row[1]
     end
     puts "Number of rows returned: #{sql.affected_rows}"
@@ -72,11 +70,11 @@ def test_instance_lvl_each_row
 end
 
 def test_instance_lvl_each_hash
-  puts "test_instance_lvl_each_hash: select * from customer limit 2"
-  WIKK::SQL::connect(@config) do |sql|
-    sql.each_hash("select * from customer limit 2") do |row|
-      puts "Row"
-      row.each do |k,v|
+  puts 'test_instance_lvl_each_hash: select * from customer limit 2'
+  WIKK::SQL.connect(@config) do |sql|
+    sql.each_hash('select * from customer limit 2') do |row|
+      puts 'Row'
+      row.each do |k, v|
         printf "  %s => %s\n", k, v
       end
     end
@@ -84,39 +82,35 @@ def test_instance_lvl_each_hash
   end
 end
 
-
-#
 def test_transaction
-  puts "test_transaction: select site_name, state, INET_NTOA(network + ..."
-  WIKK::SQL::connect(@config) do |sql|
+  puts 'test_transaction: select site_name, state, INET_NTOA(network + ...'
+  WIKK::SQL.connect(@config) do |sql|
     sql.transaction do
-      sql.each_row("select customer_id, site_name from customer limit 1") do |row|
+      sql.each_row('select customer_id, site_name from customer limit 1') do |row|
         printf "%s, %s\n", row[0], row[1]
       end
-      sql.each_row("select distribution_id, site_name from distribution limit 1") do |row|
+      sql.each_row('select distribution_id, site_name from distribution limit 1') do |row|
         printf "%s, %s\n", row[0], row[1]
       end
     end
   end
 end
 
-
-puts "*****Test Class level each row********"
+puts '*****Test Class level each row********'
 test_class_lvl_each_row
 puts
-puts "*****Test Class level each hash********"
+puts '*****Test Class level each hash********'
 test_class_lvl_each_hash
 puts
-puts "*****Test Class level each sym********"
+puts '*****Test Class level each sym********'
 test_class_lvl_each_sym
 puts
-puts "*****Test each row********"
+puts '*****Test each row********'
 test_class_lvl_each_hash_with_table_names
 puts
-puts "*****Test get fields********"
+puts '*****Test get fields********'
 test_instance_lvl_get_fields
 puts
-puts "*****Test transaction 1 customer, 1 distribution ********"
+puts '*****Test transaction 1 customer, 1 distribution ********'
 test_transaction
 puts
-
